2cd21603d8448b7572e1e2e79fc7c1d05dbbcabd
David Pett <dpett@folklore.digital>
2016-02-10 19:13:22 -0600

    added config

--- blueprints/ember-smooch/index.js
+++ blueprints/ember-smooch/index.js
@@ -1,7 +1,10 @@
 /*jshint node:true*/
 
-var chalk = require('chalk');
 var EOL = require('os').EOL;
+var chalk = require('chalk');
+var fs = require('fs-extra');
+var Promise = require('rsvp');
+var readFile = Promise.denodeify(fs.readFile);
 
 module.exports = {
   description: 'ember-smooch installation blueprint',
@@ -17,6 +20,7 @@ module.exports = {
       email: null,
       properties: null
     };
+
     return self.addPackagesToProject([{
       name: 'ember-browserify',
       target: '1.1.6'
@@ -31,6 +35,22 @@ module.exports = {
       var output = EOL;
       output += chalk.yellow('Ember Smooch') + ' has been installed. Please configure your smooch appToken in ' + chalk.green('config/environment.js') + EOL;
       console.log(output);
+    });
+  },
+
+  addToConfig: function (key, value) {
+    var self = this;
+    return this.fileContains('config/environment.js', key + ':').then(function (contains) {
+      if (contains) { return true; }
+
+      var options = { after: '    environment: environment,' + EOL };
+      return self.insertIntoFile('config/environment.js', '    ' + key + ': ' + value + ',', options);
+    });
+  },
+
+  fileContains: function (filePath, snippet) {
+    return readFile(filePath).then(function (fileContents) {
+      return fileContents.toString().indexOf(snippet) !== -1;
     });
   }
 };
