aedddbf381e2d622c3e673dcfe35cb24d19becd2
David Pett <dpett@folklore.digital>
2016-03-30 17:08:55 -0500

    fixed build

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 /* jshint node: true */
 'use strict';
 
-const path = require('path');
-
 module.exports = {
-  name: 'ember-smooch',
-
-  blueprintsPath() {
-    return path.join(__dirname, 'blueprints');
-  }
+  name: 'ember-smooch'
 };
