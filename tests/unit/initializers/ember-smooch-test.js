0c0d52e42c588c7be5199d718035ba1d4a96353b
David Pett <dpett@folklore.digital>
2016-03-31 11:55:28 -0500

    fixed test path to initializer

--- tests/unit/initializers/ember-smooch-test.js
+++ tests/unit/initializers/ember-smooch-test.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import EmberSmoochInitializer from '../../../initializers/ember-smooch';
+import EmberSmoochInitializer from '../../../initializers/browser/ember-smooch';
 import { module, test } from 'qunit';
 
 let application;
