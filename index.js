ab2b25f8feb81e1fa9b5f4901e59fa933ded61e5
David Pett <dpett@folklore.digital>
2016-03-31 11:47:48 -0500

    made fastboot ready

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 /* jshint node: true */
 'use strict';
 
+var filterInitializers = require('./lib/broccoli/filter-initializers');
+
 module.exports = {
-  name: 'ember-smooch'
+  name: 'ember-smooch',
+
+  preconcatTree: function(tree) {
+    if (process.env.EMBER_CLI_FASTBOOT) {
+      return filterInitializers(tree, 'browser', this.app.name);
+    }
+    return tree;
+  }
 };
