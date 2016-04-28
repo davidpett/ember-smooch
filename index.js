cddd1efcea8ff10e451fbe1edaae9042524761ef
David Pett <dpett@folklore.digital>
2016-04-28 08:10:09 -0500

    fastboot fixing

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 /* jshint node: true */
 'use strict';
 
-var filterInitializers = require('fastboot-filter-initializers');
-
 module.exports = {
-  name: 'ember-smooch',
-
-  preconcatTree: function(tree) {
-    return filterInitializers(tree, this.app.name);
-  }
+  name: 'ember-smooch'
 };
