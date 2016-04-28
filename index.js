7b490e1bdac2358ffa68b00c3ac4217ba02d2c85
David Pett <dpett@folklore.digital>
2016-04-28 08:20:08 -0500

    filters back

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 /* jshint node: true */
 'use strict';
 
+var filterInitializers = require('fastboot-filter-initializers');
+
 module.exports = {
-  name: 'ember-smooch'
+  name: 'ember-smooch',
+
+  preconcatTree: function(tree) {
+    return filterInitializers(tree, this.app.name);
+  }
 };
